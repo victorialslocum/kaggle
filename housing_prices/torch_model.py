bb90deedb3fac372d6d4818aa60e06a2ec10eae0
victoria <victorialslocum@gmail.com>
2022-05-13 18:00:05 -0500

    idk dude

--- housing_prices/torch_model.py
+++ housing_prices/torch_model.py
@@ -1,19 +1,15 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, normalize
 
 df = pd.read_csv(r'/home/victoriaslocum/kaggle/housing_prices/data/train.csv')
 df_test = pd.read_csv(r'/home/victoriaslocum/kaggle/housing_prices/data/test.csv')
 
 # list of features to remove: MiscFeature, Fence, PoolQC, FireplaceQu, Alley - too many null values
-df.drop(columns=['MiscFeature', 'Fence', 'PoolQC', 'FireplaceQu', 'Alley'], inplace=True)
+df.drop(columns=['Id','MiscFeature', 'Fence', 'PoolQC', 'FireplaceQu', 'Alley'], inplace=True)
 
 #turn obj columns into numbers
 obj_col = df.select_dtypes('object').columns
@@ -36,17 +32,53 @@ for col in df.columns:
 for col in null_col:
     df[col] = df[col].fillna(df[col].median())
 
+# FOR TEST DATA
+# list of features to remove: MiscFeature, Fence, PoolQC, FireplaceQu, Alley - too many null values
+x_test = df_test.drop(columns=['Id', 'MiscFeature', 'Fence', 'PoolQC', 'FireplaceQu', 'Alley'])
+
+#turn obj columns into numbers
+t_obj_col = x_test.select_dtypes('object').columns
+
+for col in t_obj_col:
+    map_list = {}
+    i = 0
+    for value in x_test[col].unique():
+        map_list[value] = i
+        i += 1
+    x_test[col] = x_test[col].map(map_list)
+
+#get rid of remaining null values with median
+t_null_col = []
+for col in x_test.columns:
+    null_val = x_test[col].isnull().sum()
+    if null_val != 0:
+        t_null_col.append(col)
+
+for col in t_null_col:
+    x_test[col] = x_test[col].fillna(x_test[col].median())
+
+# get datasets for x, y 
 x = df.drop(columns=['SalePrice'])
 y = df['SalePrice']
 
+# normalize data
 names = x.columns
-scaler = MinMaxScaler()
-d = scaler.fit_transform(x)
+# scaler = MinMaxScaler()
+# d = scaler.fit_transform(x)
+# x = pd.DataFrame(d, columns=names)
+
+d = normalize(x)
 x = pd.DataFrame(d, columns=names)
 
-#split data into train and validate
-x_train, x_validate, y_train, y_validate=train_test_split(x, y, test_size=0.20, random_state=42,shuffle=True)
+# scaler = MinMaxScaler()
+# d_test = scaler.fit_transform(x_test)
+# x_test = pd.DataFrame(d_test, columns=names)
+d_test = normalize(x_test)
+x_test = pd.DataFrame(d_test, columns=names)
 
+#split data into train and validate and test
+x_train, x_validate, y_train, y_validate=train_test_split(x, y, test_size=0.20, random_state=42,shuffle=True)
+x_test = torch.Tensor(x_test.to_numpy())
 x_train, x_validate = torch.Tensor(x_train.to_numpy()),torch.Tensor(x_validate.to_numpy())
 y_train, y_validate = torch.Tensor(y_train.to_numpy()), torch.Tensor(y_validate.to_numpy())
 
@@ -62,13 +94,13 @@ class Model(nn.Module):
         return outputs
 
 epochs = 10000
-input_dim = 75
+input_dim = 74
 output_dim = 1
 learning_rate = 0.2
 
 model = Model(input_dim, output_dim)
 
-criterion = nn.MSELoss(reduction='sum')
+criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 losses = []
@@ -76,11 +108,11 @@ losses = []
 for epoch in range(epochs):
     optimizer.zero_grad()
     outputs = model(x_train)
-    loss = criterion(outputs.squeeze(1), y_train)
+    loss = criterion(outputs.squeeze(), y_train)
     loss.backward()
     optimizer.step()
 
-    correct = np.sum(outputs.detach().numpy() == y_train)
+    correct = np.sum(outputs.squeeze().detach().numpy().round() == y_train)
     total = y_train.size(0)
     accuracy = 100 * (correct/total)
 
@@ -92,22 +124,21 @@ for epoch in range(epochs):
 
 val_losses = []
 with torch.no_grad():
-    test_outputs = model(x_validate)
-    val_loss = criterion(test_outputs.squeeze(1), y_validate)
-    correct = np.sum(test_outputs.detach().numpy() == y_validate)
+    val_outputs = model(x_validate)
+    val_loss = criterion(val_outputs.squeeze(), y_validate)
+    correct = np.sum(val_outputs.detach().numpy() == y_validate)
     total = y_validate.size(0)
     accuracy = 100 * (correct/total)
 
     val_losses.append(val_loss.item())
 
-    print(test_outputs)
     print(f"Test - Loss: {val_loss.item():.2f}, Accuracy: {accuracy:.2f}")
 
+with torch.no_grad():
+    test_outputs = model(x_test)
+    print(test_outputs)
 
-
-# id_column = df_test['PassengerId']
-# final_df = pd.DataFrame(id_column)
-# final_df['Survived'] = test_outputs.round().numpy().astype(np.int32)
-
-# # filepath = Path('/home/victoriaslocum/kaggle/titanic_ml/data/final.csv')
-# final_df.to_csv('/home/victoriaslocum/kaggle/titanic_ml/data/submission.csv', index=np.False_)
+id_column = df_test['Id']
+final_df = pd.DataFrame(id_column)
+final_df['SalePrice'] = test_outputs.squeeze().numpy().astype(np.int32)
+final_df.to_csv('/home/victoriaslocum/kaggle/housing_prices/data/submission.csv', index=np.False_)
