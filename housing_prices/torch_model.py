f83fff106f5a0c010c409572a6186bce52d83c60
victoria <victorialslocum@gmail.com>
2022-05-18 09:20:37 -0500

    better

--- housing_prices/torch_model.py
+++ housing_prices/torch_model.py
@@ -58,45 +58,45 @@ for col in t_null_col:
     x_test[col] = x_test[col].fillna(x_test[col].median())
 
 # get datasets for x, y 
-x = df.drop(columns=['SalePrice'])
-y = df['SalePrice']
+x_columns = list(df.drop(columns=['SalePrice']))
+y_columns = ['SalePrice']
 
-# normalize data
-names = x.columns
-# scaler = MinMaxScaler()
-# d = scaler.fit_transform(x)
-# x = pd.DataFrame(d, columns=names)
+means, maxs, mins = dict(), dict(), dict()
+for col in df:
+    means[col] = df[col].mean()
+    maxs[col] = df[col].max()
+    mins[col] = df[col].min()
+df = (df - df.mean()) / (df.max() - df.min())
 
-d = normalize(x)
-x = pd.DataFrame(d, columns=names)
+for col in x_test.columns:
+    x_test[col] = (x_test[col] - means[col]) / (maxs[col] - mins[col])
 
-# scaler = MinMaxScaler()
-# d_test = scaler.fit_transform(x_test)
-# x_test = pd.DataFrame(d_test, columns=names)
-d_test = normalize(x_test)
-x_test = pd.DataFrame(d_test, columns=names)
+x_df = pd.DataFrame(df, columns=x_columns)
+y_df = pd.DataFrame(df, columns=y_columns)
+x_train = torch.tensor(x_df.values, dtype=torch.float)
+y_train = torch.tensor(y_df.values, dtype=torch.float)
 
-#split data into train and validate and test
-x_train, x_validate, y_train, y_validate=train_test_split(x, y, test_size=0.20, random_state=42,shuffle=True)
-x_test = torch.Tensor(x_test.to_numpy())
-x_train, x_validate = torch.Tensor(x_train.to_numpy()),torch.Tensor(x_validate.to_numpy())
-y_train, y_validate = torch.Tensor(y_train.to_numpy()), torch.Tensor(y_validate.to_numpy())
+x_test = torch.tensor(x_test.values, dtype=torch.float)
 
 
 class Model(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(Model, self).__init__()
-        self.layer_1 = nn.Linear(input_dim, 20)
-        self.layer_2 = nn.Linear(20, output_dim)
+        self.layer_1 = nn.Linear(input_dim, 500)
+        self.layer_2 = nn.Linear(500, 1000)
+        self.layer_3 = nn.Linear(1000, 200)
+        self.layer_4 = nn.Linear(200, output_dim)
     def forward(self, x): 
         outputs = self.layer_1(x)
         outputs = self.layer_2(outputs)
+        outputs = self.layer_3(outputs)
+        outputs = self.layer_4(outputs)
         return outputs
 
-epochs = 10000
+epochs = 500
 input_dim = 74
 output_dim = 1
-learning_rate = 0.2
+learning_rate = 0.0001
 
 model = Model(input_dim, output_dim)
 
@@ -108,37 +108,26 @@ losses = []
 for epoch in range(epochs):
     optimizer.zero_grad()
     outputs = model(x_train)
-    loss = criterion(outputs.squeeze(), y_train)
+    loss = criterion(outputs, y_train)
     loss.backward()
     optimizer.step()
 
-    correct = np.sum(outputs.squeeze().detach().numpy().round() == y_train)
-    total = y_train.size(0)
-    accuracy = 100 * (correct/total)
-
     losses.append(loss.item())
 
-    if epoch%1000==0 or epoch==0:
+    if epoch%100==0 or epoch==0:
         # print(outputs)
-        print(f"Train - Loss: {loss.item():.2f}, Accuracy: {accuracy:.2f}")
-
-val_losses = []
-with torch.no_grad():
-    val_outputs = model(x_validate)
-    val_loss = criterion(val_outputs.squeeze(), y_validate)
-    correct = np.sum(val_outputs.detach().numpy() == y_validate)
-    total = y_validate.size(0)
-    accuracy = 100 * (correct/total)
-
-    val_losses.append(val_loss.item())
-
-    print(f"Test - Loss: {val_loss.item():.2f}, Accuracy: {accuracy:.2f}")
+        print(f"Train - Loss: {loss.item():.6f}")
 
 with torch.no_grad():
     test_outputs = model(x_test)
     print(test_outputs)
+    result = pd.DataFrame(test_outputs.data.numpy(), columns=['SalePrice'])
+    result['SalePrice'] = result['SalePrice'] * (maxs['SalePrice'] - mins['SalePrice']) + means['SalePrice']
 
-id_column = df_test['Id']
-final_df = pd.DataFrame(id_column)
-final_df['SalePrice'] = test_outputs.squeeze().numpy().astype(np.int32)
-final_df.to_csv('/home/victoriaslocum/kaggle/housing_prices/data/submission.csv', index=np.False_)
+    id_column = df_test['Id']
+    result['Id'] = id_column
+
+    result = pd.DataFrame(result, columns=['Id', 'SalePrice'])
+    print(result)
+
+    result.to_csv('/home/victoriaslocum/kaggle/housing_prices/data/submission.csv', index=np.False_)
